a836cc8c116cfec751a37eb3822e78e9a8ea9e49
nabilmersni <nabilmersni123@gmail.com>
2024-12-06 14:56:10 +0100

    Dependency Injection with get_it

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -30,61 +30,65 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Sign in.",
-                style: TextStyle(
-                  // color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              AuthField(
-                hintText: "Email",
-                controller: emailController,
-              ),
-              const SizedBox(height: 15),
-              AuthField(
-                hintText: "Password",
-                controller: passwordController,
-                isObscuredText: true,
-              ),
-              const SizedBox(height: 20),
-              AuthGradientButton(
-                title: "Sign In",
-                onPresesed: () {},
-              ),
-              const SizedBox(height: 20),
-              Text.rich(
-                TextSpan(
-                  text: 'Don\'t have an account? ',
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(
-                            context,
-                            SignupPage.route(),
-                          );
-                        },
-                      text: "Sign Up",
-                      style: const TextStyle(
-                        color: AppPallete.gradient2,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Sign in.",
+                    style: TextStyle(
+                      // color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 30),
+                  AuthField(
+                    hintText: "Email",
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: "Password",
+                    controller: passwordController,
+                    isObscuredText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  AuthGradientButton(
+                    title: "Sign In",
+                    onPresesed: () {},
+                  ),
+                  const SizedBox(height: 20),
+                  Text.rich(
+                    TextSpan(
+                      text: 'Don\'t have an account? ',
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                context,
+                                SignupPage.route(),
+                              );
+                            },
+                          text: "Sign Up",
+                          style: const TextStyle(
+                            color: AppPallete.gradient2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -34,75 +34,79 @@ class _SignupPageState extends State<SignupPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Sign up.",
-                style: TextStyle(
-                  // color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30),
-              AuthField(
-                hintText: "Name",
-                controller: nameController,
-              ),
-              const SizedBox(height: 15),
-              AuthField(
-                hintText: "Email",
-                controller: emailController,
-              ),
-              const SizedBox(height: 15),
-              AuthField(
-                hintText: "Password",
-                controller: passwordController,
-                isObscuredText: true,
-              ),
-              const SizedBox(height: 20),
-              AuthGradientButton(
-                title: "Sign Up",
-                onPresesed: () {
-                  if (formKey.currentState!.validate()) {
-                    context.read<AuthBloc>().add(
-                          AuthSignUp(
-                            name: nameController.text.trim(),
-                            email: emailController.text.trim(),
-                            password: passwordController.text.trim(),
-                          ),
-                        );
-                  }
-                },
-              ),
-              const SizedBox(height: 20),
-              Text.rich(
-                TextSpan(
-                  text: 'Already have an account? ',
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.pop(
-                            context,
-                          );
-                        },
-                      text: "Sign In",
-                      style: const TextStyle(
-                        color: AppPallete.gradient2,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Sign up.",
+                    style: TextStyle(
+                      // color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 30),
+                  AuthField(
+                    hintText: "Name",
+                    controller: nameController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: "Email",
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: "Password",
+                    controller: passwordController,
+                    isObscuredText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  AuthGradientButton(
+                    title: "Sign Up",
+                    onPresesed: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AuthBloc>().add(
+                              AuthSignUp(
+                                name: nameController.text.trim(),
+                                email: emailController.text.trim(),
+                                password: passwordController.text.trim(),
+                              ),
+                            );
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Text.rich(
+                    TextSpan(
+                      text: 'Already have an account? ',
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                        TextSpan(
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pop(
+                                context,
+                              );
+                            },
+                          text: "Sign In",
+                          style: const TextStyle(
+                            color: AppPallete.gradient2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
